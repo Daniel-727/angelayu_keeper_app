3c75b6d2b90117e90a626749b5d508e4184c83a1
Daniel Li <zaoster777@gmail.com>
2022-04-13 14:07:37 -0500

    finished look of input component, still need to add functionality

--- src/components/InputTemplate.jsx
+++ src/components/InputTemplate.jsx
@@ -1,1 +1,17 @@
 import React from 'react';
+
+function InputTemplate() {
+
+
+
+    
+    return(
+        <form>
+            <input placeholder='Title'></input>
+            <textarea placeholder='Take a note...'></textarea>
+            <button>Add</button>
+        </form>
+    )
+}
+
+export default InputTemplate;
