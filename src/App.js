0af787d697e6c63cce4b385b71cac1fc923e0786
Daniel Li <zaoster777@gmail.com>
2022-04-14 14:28:27 -0500

    created Note component and added the addNote functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import InputTemplate from './components/InputTemplate';
+import Note from './components/Note';
 
 function App() {
+
+    const [notes, setNotes] = React.useState([]);//notes is an array that will hold the *not sure how to describe it*
+
+    function addNote(note) { //I have successfully passed in my note object from the InputTemplate component
+        setNotes([...notes, note]); //adding my note object into the notes array
+    }
+
+
   return (
     <>
         <Header/>
-        <InputTemplate />
+        <InputTemplate click={addNote}/>
+        {notes.map((note,i) => {
+            return <Note key={i} title={note.title} content={note.content} />
+        })}
         <Footer/>
     </>
   );
--- src/components/InputTemplate.jsx
+++ src/components/InputTemplate.jsx
@@ -1,15 +1,52 @@
 import React from 'react';
 
-function InputTemplate() {
+function InputTemplate(props) {
 
+    const [note, setNote] = React.useState({
+        title: '',
+        content: ''
+    }); //note is an object that has a title and content (which are empty strings)
 
+    function handleChange(e){
+        const {name, value} = e.target; //e.target is an object, if I want to desctucture it, it must be with {} instead of [] like an array
+        if (name === 'title') {
+            setNote(prevNote => {
+                return (
+                    {
+                        title: value,
+                        content: prevNote.content
+                    }
+                );
+            });
+        } else {
+            setNote(prevNote => {
+                return (
+                    {
+                        title: prevNote.title,
+                        content: value
+                    }
+                );
+            });
+        }
+    }
 
-    
+    function handleClick(e){ //once handleClick is executed we want our addNote function from App to execute as well
+        
+        props.click(note);//this is the addNote function we passed in through props */
+
+        e.preventDefault(); //since this button is in a form, this prevents the default form behavior of refreshing the page after 'submitting' a form
+    }
+
     return(
-        <form>
-            <input placeholder='Title'></input>
-            <textarea placeholder='Take a note...'></textarea>
-            <button>Add</button>
+        <form default>
+            <input 
+                onChange={handleChange}placeholder='Title'
+                name='title'>
+            </input>
+            <textarea 
+                onChange={handleChange}placeholder='Take a note...' name='content'>
+            </textarea>
+            <button onClick={handleClick}>Add</button> {/*we must use an arrow function to pass the global event into our handleClick function for some reason, ask a friend about this (got rid of the code for the because I don't actually need it, but still ask friend)*/}
         </form>
     )
 }
